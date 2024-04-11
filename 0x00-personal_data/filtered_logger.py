f20eb423afa82d9e7faafbcd4c9711eb2bc849cd
Rophpad <rophenp@gmail.com>
2024-04-11 14:15:18 +0100

    Log formatter

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""logger functions"""
+import re
+from typing import List
+
+
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: st
+                ) -> str:
+    """Return obfuscated log message"""
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}',
+                         message
+                         )
+        return message
