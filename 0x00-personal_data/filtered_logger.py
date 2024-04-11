5cddb40151bfebf6e439d8f11b50e19075974829
Rophpad <rophenp@gmail.com>
2024-04-11 14:39:50 +0100

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """logger functions"""
 import re
+import logging
 from typing import List
 
 
@@ -17,3 +18,21 @@ def filter_datum(
                          message
                          )
         return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redaction Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filter values in incoming log records """
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
