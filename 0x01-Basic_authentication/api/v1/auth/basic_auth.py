c13d50fd1a038a5ebefed817a0cf7a414a244f03
Rophpad <rophenp@gmail.com>
2024-04-16 18:05:04 +0100

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,7 +10,8 @@ class BasicAuth(Auth):
     """ class for Basic Authentification
     """
     def extract_base64_authorization_header(
-            self, authorization_header: str
+            self,
+            authorization_header: str
     ) -> str:
         """ Get encoded header
         """
@@ -19,3 +20,20 @@ class BasicAuth(Auth):
            or (authorization_header[0:6] != "Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+    ) -> str:
+        """ Get the docoded value of a Base64 string
+        """
+        b64_auth = base64_authorization_header
+        if (not b64_auth)\
+           or (not isinstance(b64_auth, str)):
+            return None
+        try:
+            to_decode = b64_auth.encode('utf-8')
+            decoded = base64.b64decode(to_decode)
+            return decoded.decode('utf-8')
+        except Exception:
+            return None
