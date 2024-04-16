be73f5a4e3df147c6cff9da920e8c5f8467bee97
Rophpad <rophenp@gmail.com>
2024-04-16 22:09:40 +0100

    Basic - User object and Overload current_user

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Basic Authentification
 """
 
+from typing import TypeVar
 from api.v1.auth.auth import Auth
 import base64
 
@@ -51,3 +52,40 @@ class BasicAuth(Auth):
             return (None, None)
         email, password = decoded_b64_auth.split(':')
         return (email, password)
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str
+    ) -> TypeVar('User'):
+        """ Gets User instance based on his email
+            and password
+        """
+        if (not user_email) or (not isinstance(user_email, str))\
+           or (not user_pwd) or (not isinstance(user_pwd, str)):
+            return None
+        try:
+            users = User.search({"email": user_email})
+            if not users or len(users) == 0:
+                return None
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+            return None
+        except Exception:
+            return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Retrieves User instance for a request
+        """
+        auth_header = self.authorization_header(request)
+        if auth_header is not None:
+            token = self.extract_base64_authorization_header(auth_header)
+            if token is not None:
+                decoded = self.decode_base64_authorization_header(token)
+                if decoded is not None:
+                    email, password = self.extract_user_credentials(decoded)
+                    if email is not None:
+                        return self.user_object_from_credentials(
+                                email, password)
+        return
