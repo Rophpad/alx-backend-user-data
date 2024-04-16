9afd6765c5961e975247bb182878341feec9f57a
Rophpad <rophenp@gmail.com>
2024-04-16 17:17:02 +0100

    Handle Request validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -22,9 +22,12 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """ Handle authentification header
         """
-        return None
+        header = request.headers.get('Authorization')
+        if (not header):
+            return None
+        return header
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Current User request ...
+        """ Current User request ...
         """
         return None
