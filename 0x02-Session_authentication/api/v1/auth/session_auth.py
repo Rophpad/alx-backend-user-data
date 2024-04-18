2b4e411bce7779ee0acb2d9947b314c3fe219f8a
Rophpad <rophenp@gmail.com>
2024-04-18 11:03:57 +0100

    change user_id_by_session_id initialization

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,9 +9,11 @@ class SessionAuth(Auth):
     """ Class to handle
         Session authentification
     """
-    # self.user_id_by_session_id = {}
+    self.user_id_by_session_id = {}
+    """
     def __init__(self):
         self.user_id_by_session_id = {}
+    """
 
     def create_session(self, user_id: str = None) -> str:
         """ Creates a Session ID for user_id
