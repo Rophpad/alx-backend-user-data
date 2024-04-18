b36578e5b5eb32e331bc10ee7dfa44da51979e3d
Rophpad <rophenp@gmail.com>
2024-04-18 11:00:47 +0100

    [+] add user_id_by_session_id and create_session()

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,21 @@
 """ Session Authentification
 """
 from .auth import Auth
-
+from uuid import uuid4
 
 class SessionAuth(Auth):
     """ Class to handle
         Session authentification
     """
+    # self.user_id_by_session_id = {}
+    def __init__(self):
+        self.user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a Session ID for user_id
+        """
+        if (not user_id) or (not isinstance(user_id, str)):
+            return None
+        id = uuid4()
+        self.user_id_by_session_id[str(id)] = user_id
+        return str(id)
